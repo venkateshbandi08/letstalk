70167b17bb22d71566a17b17db8c684d68fb2690
venkatesh bandi <100306737+venkateshbandi08@users.noreply.github.com>
2024-06-27 04:17:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,10 +7,7 @@ const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
 
-const corsOptions = {
-  origin: ['http://localhost:3000', 'https://letstalk-v08.vercel.app'],
-};
-app.use(cors(corsOptions));
+app.use(cors('*'));
 app.use(express.json());
 
 mongoose.set("strictQuery", true);
